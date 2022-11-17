bce07aabd7f919a684aaf02e289691f49be363c5
Venkatesh Belday <belvenkatesh@gmail.com>
2022-11-17 15:26:02 -0500

    update genre filter

--- src/getFilteredMovies.js
+++ src/getFilteredMovies.js
@@ -1,12 +1,31 @@
 export default (movies, url, user_age) => {
-  const extractedFilter = url
+  console.log("🚀 ~ file: getFilteredMovies.js ~ line 2 ~ movies", movies);
+  const filtersApplied = url
     .split("?")[1]
     .split("&")
     .map((each) => {
       const param = each.replace("%22", "").split("=");
       return { [param[0]]: param[1].replace("%22", "") };
     });
-  console.log(user_age);
 
-  return movies;
+  let filteredMovies = [];
+  const seletedGenre = filtersApplieds[2]?.genres.toLowerCase();
+
+  if (seletedGenre) {
+    filteredMovies = movies.filter((movie) => {
+      console.log(
+        "🚀 ~ file: getFilteredMovies.js ~ line 20 ~ filteredMovies=movies.filter ~ movie",
+        movie.genres
+      );
+      if (movie.genres.toLowerCase().indexOf(seletedGenre) > -1) {
+        return true;
+      }
+    });
+  }
+  console.log(
+    "🚀 ~ file: getFilteredMovies.js ~ line 24 ~ filteredMovies",
+    filteredMovies
+  );
+
+  return filteredMovies;
 };
