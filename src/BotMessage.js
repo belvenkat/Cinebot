d43d56f4c028ab8605336135054bd6148d7f9f66
belvenkat <119678287+cinebot22@users.noreply.github.com>
2022-12-08 20:54:51 +0530

    updated bot pic

--- src/BotMessage.js
+++ src/BotMessage.js
@@ -23,7 +23,8 @@ const BotMessage = ({ text, user_name, user_age }) => {
         <Avatar
           src={
             <Image
-              src="https://raw.githubusercontent.com/belvenkat/Cinebot/main/cinebot.jpg"
+            src="https://drive.google.com/file/d/1B_J8ohZaSUih7lXuck9mTJORdJzMnMqT/view?usp=share_link"
+             // src="https://raw.githubusercontent.com/belvenkat/Cinebot/main/cinebot.jpg"
               style={{ width: 32 }}
             />
           }
